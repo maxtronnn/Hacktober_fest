c7f7cc2c225b974799e397327d840280673e2875
marcury122 <mehuljain0503@gmail.com>
2023-10-06 20:56:01 +0530

    new commit

---
+++ new.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    for (int testc = 1; testc <= t; ++testc)
+    {
+        cout << "hello" << endl;
+    }
+
+    return 0;
+}
